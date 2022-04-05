0760ea62418887509bc59c5b0f1986a955ab8dc6
Ben Finley <bben10hi@gmail.com>
2022-04-04 22:35:32 -0400

    created a player object with tests

---
+++ src/gameLogic/player.js
@@ -0,0 +1,81 @@
+/* eslint-disable no-undef */
+import gameBoard from './gameboard';
+
+const player = () => {
+  const board = gameBoard();
+  board.createGameBoard();
+  let ships = {}
+
+  const placeShip = (type, orientation, start) => {
+    let length;
+    switch (type) {
+      case "Carrier":
+        length = 5;
+        break;
+      case "Battleship":
+        length = 4;
+        break;
+      case "Destroyer":
+        length = 2;
+        break;
+      default:
+        length = 3;
+        break;
+    }
+    let end = []
+    if (orientation === 'H') {
+      end[0] = start[0];
+      end[1] = start[1] + length;
+    }
+    if (orientation === 'V') {
+      end[0] = start[0] + length;
+      end[1] = start[1];
+    }
+    ships[type] = {start, end, "sunk": false}
+    board.placeShip(start, end);
+  }
+
+  const getGameBoard = () => {
+    return board.getGameBoard()
+  }
+
+  const getHit = (x,y) => {
+    board.getHit(x,y);
+    checkForSunkShips();
+  }
+
+  const checkForSunkShips = () => {
+    Object.entries(ships).forEach( ship => {
+      let sunk = true;
+      if (ship[1].start[0] === ship[1].end[0]){ // horizzontal
+        for (let i = ship[1].start[1]; i <= ship[1].end[1]; i++) {
+          if (getGameBoard()[ship[1].start[0]][i] === 1) {
+            sunk = false;
+          }
+        }
+      }
+      if (ship[1].start[1] === ship[1].end[1]){ // horizzontal
+        for (let i = ship[1].start[0]; i <= ship[1].end[0]; i++) {
+          if (getGameBoard()[ship[1].start[1]][i] === 1) {
+            sunk = false;
+          }
+        }
+      }
+      ships[ship[0]].sunk = sunk;
+    })
+  }
+
+  const getSunkShips = () => {
+    let sunkShips = {}
+    Object.entries(ships).forEach( ship => {
+      if (ship[1].sunk) {
+        sunkShips[ship[0]] = ship[1];
+      }
+    })
+    return sunkShips;
+  }
+
+  return { getGameBoard, placeShip, getHit, getSunkShips }
+};
+
+module.exports = player;
---
+++ src/gameLogic/player.test.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-undef */
+import player from "./player";
+
+test('place horizontal ships', () => {
+  const playerOne = player(1);
+  playerOne.placeShip('battleship', 'H', [2,3]);
+  for (let i = 3; i <= 6; i++) {
+    expect(playerOne.getGameBoard()[2][i]).toBe(1);
+    expect(playerOne.getGameBoard()[3][i]).toBe(0);
+  }
+})
+
+test('place vertical ships', () => {
+  const playerOne = player(1);
+  playerOne.placeShip('battleship', 'V', [2,3]);
+  for (let i = 2; i <= 5; i++) {
+    expect(playerOne.getGameBoard()[i][3]).toBe(1);
+    expect(playerOne.getGameBoard()[i][4]).toBe(0);
+  }
+})
+
+test('place two ships', () => {
+  const playerOne = player(1);
+  playerOne.placeShip('battleship', 'V', [2,3]);
+  playerOne.placeShip('submarine', 'H', [5,6]);
+  for (let i = 2; i <= 5; i++) {
+    expect(playerOne.getGameBoard()[i][3]).toBe(1);
+    expect(playerOne.getGameBoard()[i][4]).toBe(0);
+  }
+  for (let i = 6; i <= 9; i++) {
+    expect(playerOne.getGameBoard()[5][i]).toBe(1);
+    expect(playerOne.getGameBoard()[6][i]).toBe(0);
+  }
+})
+
+test('get hit and missed', () => {
+  const playerOne = player(1);
+  playerOne.getHit(3,6);
+  expect(playerOne.getGameBoard()[3][6]).toBe(2);
+})
+
+test('get hit big', () => {
+  const playerOne = player(1);
+  playerOne.placeShip('battleship', 'V', [2,3]);
+  playerOne.getHit(2,3);
+  expect(playerOne.getGameBoard()[2][3]).toBe(3);
+})
+
+test('check ships with all alive', () => {
+  const playerOne = player(1);
+  playerOne.placeShip('battleship', 'V', [2,3]);
+  playerOne.placeShip('submarine', 'H', [5,6]);
+  expect(playerOne.getSunkShips()).toMatchObject({})
+})
+
+test('check ships with one sunk', () => {
+  const playerOne = player(1);
+  playerOne.placeShip('battleship', 'V', [2,3]);
+  playerOne.placeShip('submarine', 'H', [5,6]);
+  playerOne.getHit(5,6);
+  playerOne.getHit(5,7);
+  playerOne.getHit(5,8);
+  playerOne.getHit(5,9);
+  expect(playerOne.getSunkShips()).toMatchObject({submarine: { start: [ 5, 6 ], end: [ 5, 9 ], sunk: true }})
+})
