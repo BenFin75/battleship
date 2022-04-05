602c62d1317cd8b04618b780641bb7d50eed2d87
Ben Finley <bben10hi@gmail.com>
2022-04-04 22:36:05 -0400

    created a gameboard object with tests

---
+++ src/gameLogic/gameboard.js
@@ -0,0 +1,42 @@
+/* eslint-disable no-undef */
+const gameBoard = () => {
+  let gameBoard = [];
+
+  const createGameBoard = () => {
+    for (let row = 0; row < 10; row++) {
+      let columns = [];
+      for (let column = 0; column < 10; column++) {
+        columns.push(0)
+      }
+      gameBoard.push(columns)
+    }
+  }
+
+  const placeShip = (start, end) => {
+    if (start[0] === end[0]) { //place horizontally
+      for (let i = start[1]; i <= end[1]; i++) {
+        gameBoard[start[0]][i] = 1;
+      }
+    } else { // place vertically
+      for (let i = start[0]; i <= end[0]; i++) {
+        gameBoard[i][start[1]] = 1;
+      }
+    }
+  }
+
+  const getHit = (x, y) => {
+    if (gameBoard[x][y] === 1) {
+      gameBoard[x][y] = 3;
+    } else {
+      gameBoard[x][y] = 2;
+    }
+  }
+
+  const getGameBoard = () => {
+    return gameBoard
+  }
+
+  return { createGameBoard, placeShip, getHit, getGameBoard }
+}
+
+module.exports = gameBoard;
---
+++ src/gameLogic/gameboard.test.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-undef */
+import gameBoard from "./gameboard"
+
+test('creates gameboard', () => {
+  const testBoard = gameBoard();
+  testBoard.createGameBoard();
+  expect(testBoard.getGameBoard().length).toBe(10);
+  expect(testBoard.getGameBoard()[0].length).toBe(10);
+});
+
+test('hit and miss', () => {
+  const testBoard = gameBoard();
+  testBoard.createGameBoard();
+  testBoard.getHit(0,0);
+  expect(testBoard.getGameBoard()[0][0]).toBe(2);
+});
+
+test('place ship horizontally', () => {
+  const testBoard = gameBoard();
+  testBoard.createGameBoard();
+  testBoard.placeShip([0,0], [0,3])
+  for (let i = 0; i < 4; i++) {
+    expect(testBoard.getGameBoard()[0][i]).toBe(1); // check it changed
+    expect(testBoard.getGameBoard()[1][i]).toBe(0); // check nothing else changed
+  }
+})
+
+test('place ship vertically', () => {
+  const testBoard = gameBoard();
+  testBoard.createGameBoard();
+  testBoard.placeShip([0,1], [3,1]);
+  for (let i = 0; i < 4; i++) {
+    expect(testBoard.getGameBoard()[i][1]).toBe(1); // check it changed
+    expect(testBoard.getGameBoard()[i][0]).toBe(0); // check nothing else changed
+  }
+})
